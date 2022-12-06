2d40f312dcc9f8d96e2bd8daa8b5feb54863f852
ishutinav <aleksei-ishutin@yandex.ru>
2022-12-06 21:34:46 +0400

    Исправление ошибок линтера #1

--- src/main.js
+++ src/main.js
@@ -6,11 +6,8 @@ const siteBodyElement = document.querySelector('.page-body');
 const siteHeaderElement = siteBodyElement.querySelector('.page-header');
 const sectionBoardElement = siteBodyElement.querySelector('.trip-events');
 const siteControlsFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
-
 const boardPresenter = new BoardPresenter({boardContainer: sectionBoardElement});
 
-
 render(new FilterView(), siteControlsFiltersElement);
-
 
 boardPresenter.init();
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,23 +5,22 @@ import TripView from '../view/trip-view.js';
 import AddNewPointView from '../view/add-new-point-view.js';
 import {render} from '../render.js';
 
-
 export default class BoardPresenter {
-    tripListComponent = new TripListView();
+  tripListComponent = new TripListView();
   
-    constructor({boardContainer}) {
-      this.boardContainer = boardContainer;
-    }
+  constructor({boardContainer}) {
+    this.boardContainer = boardContainer;
+  }
   
-    init() {
-        render(new EditPointView(), this.boardContainer);   
-        render(new SortView(), this.boardContainer);
-        render(this.tripListComponent, this.boardContainer);
+  init() {
+    render(new EditPointView(), this.boardContainer);   
+    render(new SortView(), this.boardContainer);
+    render(this.tripListComponent, this.boardContainer);
 
-        for (let i = 0; i < 3; i++) {
-            render(new TripView(), this.tripListComponent.getElement());
-        }
-        
-        render(new AddNewPointView(), this.boardContainer);        
+    for (let i = 0; i < 3; i++) {
+      render(new TripView(), this.tripListComponent.getElement());
     }
+
+    render(new AddNewPointView(), this.boardContainer);
+ }
 }
