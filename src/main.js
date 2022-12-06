95edfb4d1b6061c7be406406f6f3643fcb042f5b
ishutinav <aleksei-ishutin@yandex.ru>
2022-12-06 21:08:12 +0400

    Создание компонентов. Добавление презентера

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import FilterView from './view/filter-view.js';
+import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
+
+const siteBodyElement = document.querySelector('.page-body');
+const siteHeaderElement = siteBodyElement.querySelector('.page-header');
+const sectionBoardElement = siteBodyElement.querySelector('.trip-events');
+const siteControlsFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
+
+const boardPresenter = new BoardPresenter({boardContainer: sectionBoardElement});
+
+
+render(new FilterView(), siteControlsFiltersElement);
+
+
+boardPresenter.init();
