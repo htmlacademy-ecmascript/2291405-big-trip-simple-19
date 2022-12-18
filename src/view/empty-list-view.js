6c1b10757578d04ac0e108db125221d9a3819b1c
ishutinav <aleksei-ishutin@yandex.ru>
2022-12-18 18:11:59 +0400

    Добавление заглушки для пустого списка

---
+++ src/view/empty-list-view.js
@@ -0,0 +1,30 @@
+import {createElement} from '../render.js';
+
+
+function createEmptyListTemplate() {
+  return (
+    `<p class="trip-events__msg">
+        Click New Event to create your first point
+    </p>`
+  );
+}
+
+export default class EmptyListView {
+  #element = null;
+
+  get template() {
+    return createEmptyListTemplate();
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
